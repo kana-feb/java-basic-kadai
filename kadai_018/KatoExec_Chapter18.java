93eb8dab61a93acaa04602793426716d17c430e4
kana-feb <canary.sug.02@gmail.com>
2025-06-26 10:16:07 +0900

    commit

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName(null);
+		taro.commonIntroduce(taro.givenName);
+		taro.eachIntroduce();
+		
+		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
+		Ichiro.setGivenName(null);
+		Ichiro.commonIntroduce(Ichiro.givenName);
+		Ichiro.eachIntroduce();
+		
+		
+		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
+		Hanako.setGivenName(null);
+		Hanako.commonIntroduce(Hanako.givenName);
+		Hanako.eachIntroduce();
+
+	}
+
+}
